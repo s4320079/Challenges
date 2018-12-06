ab61c245f55eb77c906ac7bfbc64f114058e54da
Jacob Rogers <jacobgrantrogers@gmail.com>
2018-12-06 22:04:13 +1000

    finished knight challenge

--- python/KnightChess.py
+++ python/KnightChess.py
@@ -45,23 +45,49 @@ def isThreat(knightPosition, kingPosition) :
     xDisplacement = abs(knightPosition[0] - kingPosition[0])
     yDisplacement = abs(knightPosition[1] - kingPosition[1])
     if xDisplacement > 3 :
-        return false
+        return False
     if yDisplacement > 3 :
-        return false
+        return False
     if xDisplacement == 3 and yDisplacement == 3 :
-        return false
-    return true
+        return False
+    return True
+
+
+"""
+    returns true if the input square coresponds to checkmate
+"""
+def checkmate(square):
+    for i in range(0,3) :
+        for j in range(0,3) :
+            if square[i][i] == False :
+                return False
+    return True
     
 
 numberOfTests = int(input())
+moves = [(1,2), (2,1), (-1,2), (2,-1), (1,-2), (-2,1), (-1,-2), (-2,-1)]
 for i in range(0, numberOfTests):
-    threatendSquares = resetThreatendSquares()
+    #threatendSquares = resetThreatendSquares()
+    threatendSquares =[[False, False, False], [False, False, False], [False, False, False]]
     numberOfKnights = int(input())
     knights = []
     # load knights
     for j in range(0, numberOfKnights):
         knights.append(listToInt(str.split(input())))
     kingPosition = listToInt(str.split(input()))
+    for j in range(0, numberOfKnights):
+        if isThreat(knights[j], kingPosition):
+            for k in range(0,8) :
+                #calculate the squares in threatend square
+                #the knight threatens with the given move
+                xThreat = knights[j][0] - kingPosition[0]+ moves[k][0] + 1
+                yThreat = knights[j][1] - kingPosition[1]+ moves[k][1] + 1
+                if xThreat >=0 and xThreat <= 2 and yThreat >=0 and yThreat <= 2:
+                    threatendSquares[xThreat][yThreat] = True
+    #print (threatendSquares)
+    if(checkmate(threatendSquares)) :
+        print("YES")
+    else :
+        print("NO")
     
-
 
